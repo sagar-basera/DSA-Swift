773b54e04c7b0537cd2f4bec6b3f87e5b185225e
Sagar_Basera <sagarbasera27@gmail.com>
2023-10-07 21:22:37 +0530

    Lower bound

--- DSA/BinarySearch.swift
+++ DSA/BinarySearch.swift
@@ -40,4 +40,27 @@ class BinarySearch {
             return binarySearchRecursive(arr: arr, target: target, start: start, end: mid-1)
         }
     }
+    
+    //MARK: - Lower Bound.
+    /// The index of the first element that is not less than K.
+    /// i.e., arr[mid] >= K.
+    func lowerBound(arr: [Int], K: Int) -> Int {
+        var low = 0
+        var high = arr.count-1
+        var ans = arr.count
+        
+        while low <= high {
+            let mid = low + (high-low)/2
+            
+            if arr[mid] >= K {
+                ans = mid
+                high = mid - 1
+            }else {
+                low = mid + 1
+            }
+        }
+        
+        return ans
+    }
+    
 }
--- DSA/main.swift
+++ DSA/main.swift
@@ -16,3 +16,5 @@ print(binarySearch.binarySearch(arr: arr, target: 1))
 //MARK: - 2.Binary Search Recursive.
 print(binarySearch.binarySearchRecursive(arr: arr, target: 7, start: 0, end: arr.count-1))
 
+//MARK: - 3. Lower Bound.
+print(binarySearch.lowerBound(arr: arr, K: 22))
