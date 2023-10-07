e87a189003227ec102f890d2e8ceda7c03452c82
Sagar_Basera <sagarbasera27@gmail.com>
2023-10-07 21:27:13 +0530

    Upper bound

--- DSA/BinarySearch.swift
+++ DSA/BinarySearch.swift
@@ -63,4 +63,26 @@ class BinarySearch {
         return ans
     }
     
+    //MARK: - Upper Bound.
+    /// The index of the element that is greater than K.
+    /// i.e., arr[mid] > K.
+    func upperBound(arr: [Int], K: Int) -> Int {
+        var low = 0
+        var high = arr.count-1
+        var ans = arr.count
+        
+        while low <= high {
+            let mid = low + (high-low)/2
+            
+            if arr[mid] > K {
+                ans = mid
+                high = mid - 1
+            }else {
+                low = mid + 1
+            }
+        }
+        return ans
+    }
+    
+    
 }
--- DSA/main.swift
+++ DSA/main.swift
@@ -9,12 +9,15 @@ import Foundation
 
 let binarySearch = BinarySearch()
 
-//MARK: - 1. Binary Search Algorithm.
+//MARK: - 1.Binary Search Algorithm.
 let arr: [Int] = [1, 4, 5, 7, 9, 11, 14]
 print(binarySearch.binarySearch(arr: arr, target: 1))
 
 //MARK: - 2.Binary Search Recursive.
 print(binarySearch.binarySearchRecursive(arr: arr, target: 7, start: 0, end: arr.count-1))
 
-//MARK: - 3. Lower Bound.
+//MARK: - 3.Lower Bound.
 print(binarySearch.lowerBound(arr: arr, K: 22))
+
+//MARK: - 4.Upper Bound.
+print(binarySearch.upperBound(arr: arr, K: 7))
